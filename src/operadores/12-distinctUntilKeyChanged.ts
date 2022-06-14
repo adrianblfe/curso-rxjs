96d99aecb205e0d23d915d93b69d9e6a5eca8ccb
adrianblfe <adrianblfe@gmail.com>
2022-06-14 10:57:47 +0200

    Fin sección 6

---
+++ src/operadores/12-distinctUntilKeyChanged.ts
@@ -0,0 +1,36 @@
+import { distinctUntilKeyChanged, from } from "rxjs";
+
+
+interface Personaje {
+	nombre: string;
+}
+
+const personajes: Personaje[] = [
+	{
+		nombre: 'Megaman'
+	},
+	{
+		nombre: 'Megaman'
+	},
+	{
+		nombre: 'X'
+	},
+	{
+		nombre: 'X'
+	},
+	{
+		nombre: 'Zero'
+	},
+	{
+		nombre: 'Megaman'
+	},
+	{
+		nombre: 'Superman'
+	},
+]
+
+
+from(personajes).pipe(
+	distinctUntilKeyChanged('nombre')
+).subscribe( console.log )
+
