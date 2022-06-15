a49c782d3d55483f5e54f882e9938ebe8f2c9884
adrianblfe <adrianblfe@gmail.com>
2022-06-15 10:49:39 +0200

    Fin sección 10

---
+++ src/combinacion/03-concat.ts
@@ -0,0 +1,19 @@
+import { concat, interval, take } from "rxjs";
+
+
+
+
+
+
+const interval$ = interval(1000)
+
+
+concat(
+	interval$.pipe( take(3) ),
+	interval$.pipe( take(2) ),
+).subscribe( console.log );
+
+
+
+
+
